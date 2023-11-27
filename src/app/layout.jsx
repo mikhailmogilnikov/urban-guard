587068b9883419e6843c8b3c1ac11af0ec91a916
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2023-11-27 20:17:33 +0300

    fix: replace favicons path

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -16,26 +16,22 @@ export default function RootLayout({ children }) {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
+          href="/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon/favicon-32x32.png"
+          href="/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon/favicon-16x16.png"
+          href="/favicon-16x16.png"
         />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/favicon/safari-pinned-tab.svg"
-          color="#000000"
-        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
         <meta name="apple-mobile-web-app-title" content="Urban Guard" />
         <meta name="application-name" content="Urban Guard" />
         <meta name="msapplication-TileColor" content="#000000" />
